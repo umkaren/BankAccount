aad57d157df3bf750699430db71c2d3eb7f4306e
Karen Ngo <uminhngo@gmail.com>
2023-05-12 18:39:24 -0400

    Edited to take into account Part 2 problems. Please refer to previous commit for part 1 grading.

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,10 +1,24 @@
-public class BankAccount {
+import java.util.ArrayList;
+import java.util.Scanner;
+class BankAccount {
     private double accountBalance;
     private String accountHolder;
+    private int accountNumber;
 
-    public BankAccount(double accountBalance, String accountHolder) {
+    //first constructor that takes three parameters
+    public BankAccount(double accountBalance, String accountHolder, int accountNumber) {
         this.accountBalance = accountBalance;
         this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+    }
+
+    //second constructor that takes no parameters
+    public BankAccount() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is the name for the account?");
+        this.accountHolder = scanner.nextLine();
+        System.out.println("What is the beginning balance for the account?");
+        this.accountBalance = scanner.nextDouble();
     }
     public void deposit(double deposit) {
         accountBalance += deposit;
@@ -12,8 +26,36 @@ public class BankAccount {
     public void withdraw(double withdraw) {
         accountBalance -= withdraw;
     }
-
+    public void transfer(ArrayList<BankAccount> accounts,BankAccount recipient, double amount) {
+        boolean accountFound = false;
+        for (BankAccount account : accounts) {
+            if (account.getAccountNumber() == accountNumber) {
+                this.withdraw(amount);
+                recipient.deposit(amount);
+                accountFound = true;
+                break;
+            }
+        }
+        if (!accountFound) {
+            System.out.println("Account doesn't exist. Transfer failed.");
+        } else {
+            System.out.println("Your account balance is now $" + this.getAccountBalance());
+            System.out.println(recipient.getAccountHolder() + "'s account balance is $" + recipient.getAccountBalance());
+        }
+    }
+    //this was used in part 1 but no longer used in part 2. Refer to previous commit to see usage.
     public void printAccountDetails() {
         System.out.println(accountHolder + "'s account balance is: $" + accountBalance);
+    }
+
+    //part 2 Bank Account
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+    public int getAccountNumber() {
+        return accountNumber;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,116 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        BankAccount account1 = new BankAccount(500, "Peter Parker");
-        BankAccount account2 = new BankAccount(5000, "Gwen Stacy");
-        BankAccount account3 = new BankAccount(300, "Doctor Octopus");
+        ArrayList<BankAccount> accounts = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        BankAccount currentAccount = null;
 
-        account1.deposit(100);
-        account2.withdraw(100);
-        account3.deposit(100);
+        //creates three instances from first Bank account exercise. Modified to complete Bank Account Continuation. See previous commit for Part 1.
+        accounts.add(new BankAccount(500, "Peter Parker", 001));
+        accounts.add(new BankAccount(5000, "Gwen Stacy", 002));
+        accounts.add(new BankAccount(300, "Doctor Octopus", 003));
 
-        account1.printAccountDetails();
-        account2.printAccountDetails();
-        account3.printAccountDetails();
+        //deposits and withdrawals from part 1
+        accounts.get(0).deposit(100);
+        accounts.get(1).withdraw(100);
+        accounts.get(2).deposit(100);
+
+        //adding user input from part 2: bank account
+        System.out.println("Hello world! Welcome to the Bank of Karen!");
+        System.out.println("Are you an existing customer?");
+        System.out.println("1. Yes");
+        System.out.println("2. No");
+
+        String yesOrNo = scanner.nextLine();
+
+        //if users says they have an account
+        if (yesOrNo.equalsIgnoreCase("yes")) {
+            System.out.println("What is the name for the account?");
+            String accountName = scanner.nextLine();
+
+            boolean accountFound = false;
+            for (BankAccount account : accounts) {
+                if (account.getAccountHolder().equalsIgnoreCase(accountName)) {
+                    currentAccount = account;
+                    accountFound = true;
+                    break;
+                }
+            }
+            if (!accountFound) {
+                System.out.println("Account not found.");
+            } else {
+                System.out.println("Welcome back, " + currentAccount.getAccountHolder() + "!");
+                mainMenu(currentAccount, accounts);
+            }
+        } else if (yesOrNo.equalsIgnoreCase("no")) {
+            //ask user for input to create new account
+            System.out.println("Let's make a new account!");
+            BankAccount newAccount = new BankAccount();
+            //adds new account to accounts ArrayList
+            accounts.add(newAccount);
+            System.out.println("Your account has been created");
+            currentAccount = newAccount;
+            mainMenu(currentAccount, accounts);
+        }
+    }
+
+    //adding main menu
+    public static void mainMenu(BankAccount currentAccount, ArrayList<BankAccount> accounts) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("This is the main menu. What would you like to do today?");
+        System.out.println("1. To check account balance");
+        System.out.println("2. To make a withdrawal");
+        System.out.println("3. To make a deposit");
+        System.out.println("4. To make a transfer to another account");
+        System.out.println("0. To exit");
+
+        int choice = scanner.nextInt();
+        //set amount in case of deposit/withdrawal/transfer
+        double amount;
+        int accountNumber;
+
+        switch (choice) {
+            case 0:
+                System.out.println("Thanks for visiting! Goodbye!");
+                break;
+            case 1:
+                System.out.println("Your balance is $" + currentAccount.getAccountBalance());
+                break;
+            case 2:
+                System.out.println("How much would you like to withdraw?");
+                amount = scanner.nextDouble();
+                currentAccount.withdraw(amount);
+                System.out.println("Withdrawal completed. Your account balance is now $" + currentAccount.getAccountBalance());
+                break;
+            case 3:
+                System.out.println("How much would you like to deposit?");
+                amount = scanner.nextDouble();
+                currentAccount.deposit(amount);
+                System.out.println("Deposit completed. Your account balance is now $" + currentAccount.getAccountBalance());
+                break;
+            case 4:
+                System.out.println("Please enter the account number to transfer to");
+                accountNumber = scanner.nextInt();
+                BankAccount recipient = null;
+
+                for (BankAccount account : accounts) {
+                    if (account.getAccountNumber() == accountNumber) {
+                        recipient = account;
+                        break;
+                    }
+                }
+                if (recipient != null) {
+                    System.out.println("Please enter the amount to transfer");
+                    amount = scanner.nextDouble();
+                    currentAccount.transfer(accounts, recipient, amount);
+                } else {
+                    System.out.println("Recipient account not found.");
+                }
+                break;
+            default:
+                System.out.println("Invalid choice. Please input an option from 0-4.");
+        }
     }
 }
